1434a6ac3442427c8d73db1b8c550070903c0ac4
Kovalski <mihaenko11@mail.ru>
2023-08-02 14:20:53 +0300

    изменен путь для доступа к файлу конфигурации firestore

--- database/__init__.py
+++ database/__init__.py
@@ -7,9 +7,11 @@ import os
 
 class DataBaseConnector:
     def __init__(self):
-        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
+        absolute_config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
 
-        self._cred = credentials.Certificate(file_path)
+        config_file_path = os.environ.get('FIREBASE_CONFIG_PATH', absolute_config_file_path)
+
+        self._cred = credentials.Certificate(config_file_path)
 
         if not firebase_admin._apps:
             self._app = firebase_admin.initialize_app(self._cred)
