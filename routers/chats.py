210fcc4051928e11fbe31c6ce0d6418b6eb06fa0
KOVASLKl <mihaenko11@mail.ru>
2023-08-07 13:08:40 +0300

    Пофикшена ошибка с неправильным получением сообщений из коллекции чата

--- routers/chats.py
+++ routers/chats.py
@@ -85,10 +85,8 @@ async def get_user_chat(request: Request, chat_id: str):
 
         messages = []
 
-        for message_id in chat_ref.collection('messages').stream():
-            message_ref = chat_ref.collection('messages').document(message_id)
-            message_obj = (message_ref.get()).to_dict()
-
+        for message_doc in chat_ref.collection('messages').stream():
+            message_obj = message_doc.to_dict()
             messages.append(message_obj)
 
         chat_dict.update({'messages': messages})
