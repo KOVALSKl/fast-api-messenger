d1471b5cf8a01916d0758d86b82ab1a632d80da4
KOVASLKl <mihaenko11@mail.ru>
2023-08-10 10:12:09 +0300

    Предыдущая версия получения чата, версия с получением через имя в коллекции пользователя не работает, по причине возможности использования разных символов внутри имени чата

--- routers/chats.py
+++ routers/chats.py
@@ -58,11 +58,11 @@ async def get_all_user_chats(request: Request):
 
 
 @router.get('/{chat_id}')
-async def get_user_chat(request: Request, chat_name: str):
+async def get_user_chat(request: Request, chat_id: str):
     """
     Получение конкретного чата/диалога пользователя по идентификатору чата
     :param request: Объект запроса
-    :param chat_name: Имя чата в коллекции пользователя
+    :param chat_id: Идентификатор чата
     :return:
     """
     try:
@@ -77,18 +77,10 @@ async def get_user_chat(request: Request, chat_name: str):
         if not user_ref:
             return HTTPException(detail={'message': f"Пользователь не существует"}, status_code=400)
 
-        chat_meta_ref = user_ref.collection('chats').document(chat_name)
-        chat_meta_doc = chat_meta_ref.get()
-
-        if not chat_meta_doc.exists:
-            return HTTPException(detail={'message': f"Чата {chat_name} не существует"}, status_code=404)
-
-        chat_meta_model = ChatMeta(**(chat_meta_doc.to_dict()))
-
-        chat_ref = lib.root_collection_item_exist(database, 'chats', chat_meta_model.chat_id)
+        chat_ref = lib.root_collection_item_exist(database, 'chats', chat_id)
 
         if not chat_ref:
-            return HTTPException(detail={'message': f"Чата {chat_meta_model.chat_id} не существует"}, status_code=404)
+            return HTTPException(detail={'message': f"Чата {chat_id} не существует"}, status_code=404)
 
         chat_dict = chat_ref.get().to_dict()
         chat_model = Chat(**chat_dict)
